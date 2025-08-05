d26c4afb8df169989aeed3154de6d7bef4def63d
hao-ma-to <dom_git@outlook.com>
2025-08-05 10:17:06 -0400

    Fix test to check dominant_emotion field

--- tests/test_emotion_detection.py
+++ tests/test_emotion_detection.py
@@ -1,10 +1,10 @@
 from EmotionDetection.emotion_detection import emotion_detector
 
 def test_detect_emotion():
-    assert emotion_detector("I am glad this happened") == "joy"
-    assert emotion_detector("I am really mad about this") == "anger"
-    assert emotion_detector("I feel disgusted just hearing about this") == "disgust"
-    assert emotion_detector("I am so sad about this") == "sadness"
-    assert emotion_detector("I am really afraid that this will happen") == "fear"
+    assert emotion_detector("I am glad this happened")["dominant_emotion"] == "joy"
+    assert emotion_detector("I am really mad about this")["dominant_emotion"] == "anger"
+    assert emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"] == "disgust"
+    assert emotion_detector("I am so sad about this")["dominant_emotion"] == "sadness"
+    assert emotion_detector("I am really afraid that this will happen")["dominant_emotion"] == "fear"
 	
 	
